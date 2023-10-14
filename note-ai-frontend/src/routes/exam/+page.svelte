b8329e2a8729704f4b71ed25125090a4066b3f86
Srijan Ray <srijanray12@gmail.com>
2023-10-14 17:24:03 -0400

    fixing some merge errors and readded ability to show answers

--- note-ai-frontend/src/routes/exam/+page.svelte
+++ note-ai-frontend/src/routes/exam/+page.svelte
@@ -27,9 +27,17 @@
        }
    }
    console.log(answerChoicesAndQuestion[0][2]);
+
+   let showAnswers = false;
  </script>
 
 <h> Testing out the page component </h>
 <QuestionBlock question={answerChoicesAndQuestion[0][0]}, answerChoice1={answerChoicesAndQuestion[0][1]}, answerChoice2={answerChoicesAndQuestion[0][2]}, answerChoice3={answerChoicesAndQuestion[0][3]}, answerChoice4={answerChoicesAndQuestion[0][4]}/>
 <QuestionBlock question={answerChoicesAndQuestion[1][0]}, answerChoice1={answerChoicesAndQuestion[1][1]}, answerChoice2={answerChoicesAndQuestion[1][2]}, answerChoice3={answerChoicesAndQuestion[1][3]}, answerChoice4={answerChoicesAndQuestion[1][4]}/>
 <QuestionBlock question={answerChoicesAndQuestion[2][0]}, answerChoice1={answerChoicesAndQuestion[2][1]}, answerChoice2={answerChoicesAndQuestion[2][2]}, answerChoice3={answerChoicesAndQuestion[2][3]}, answerChoice4={answerChoicesAndQuestion[2][4]}/>
+<p></p>
+<button on:click={() => showAnswers=true}>Show Answers?</button>
+
+{#if showAnswers}
+    <p> {answerString} </p>
+{/if}
