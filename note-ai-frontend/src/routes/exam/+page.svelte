ee62f08f8dbd86fda8d86f7d4b7e0ed413399477
Srijan Ray <srijanray12@gmail.com>
2023-10-14 12:40:49 -0400

    developed JS code for parsing json of outputted gpt prompt

--- note-ai-frontend/src/routes/exam/+page.svelte
+++ note-ai-frontend/src/routes/exam/+page.svelte
@@ -1,1 +1,25 @@
+<script>
+   // import entire JSON file as a default import
+   import { question } from '$lib/data/testPrompt.json';
+
+   // import root level object as a named import
+   import { answer } from '$lib/data/testPrompt.json';
+
+
+
+   const questionString = question;
+   const answerString = answer;
+   console.log(questionString);
+   console.log(answerString);
+   const questions = questionString.split("\n\n");
+   let answerChoicesAndQuestion = new Array();
+   questions.forEach((q) => {
+       answerChoicesAndQuestion.push(q.split("\n"))});
+   console.log(questions);
+   answerChoicesAndQuestion.forEach((q) => {
+       console.log(q);
+   });
+   console.log(answerChoicesAndQuestion[0][2]);
+ </script>
+
 <h> Testing out the page component </h>
