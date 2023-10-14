883b716b39a97b65d5b15560bba8bed1d0a2d220
Srijan Ray <srijanray12@gmail.com>
2023-10-14 00:18:12 -0400

    changes to uploading a word doc

--- note-ai-frontend/src/routes/+page.svelte
+++ note-ai-frontend/src/routes/+page.svelte
@@ -1,3 +1,28 @@
+<script>
+	let files;
+
+	$: if (files) {
+		// Note that `files` is of type `FileList`, not an Array:
+		// https://developer.mozilla.org/en-US/docs/Web/API/FileList
+		console.log(files);
+
+		for (const file of files) {
+			console.log(`${file.name}: ${file.size} bytes`);
+		}
+	}
+</script>
+
 <h1>Choose a file upload type</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p> <a href="/exam">Word</a> or Pdf (unsupported) </p>
+
+<label for="avatar">Upload a Word Document:</label>
+<input accept=".docx, .doc" bind:files id="avatar" name="avatar" type="file" />
+
+{#if files}
+	<h2>Selected files:</h2>
+	{#each Array.from(files) as file, i}
+		<p>{file.name} ({file.size} bytes)</p>
+        {#await file.text() then text}
+            <p>e: {text} i: {i}</p>
+        {/await}
+	{/each}
+{/if}
