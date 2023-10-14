28783a5b7b34523e7eb8dfbecfb3537992e2a21b
Srijan Ray <srijanray12@gmail.com>
2023-10-14 16:31:52 -0400

    made new Svelte Component for Question Block and made semi-fuctional practice quiz layout

---
+++ note-ai-frontend/src/routes/QuestionBlock.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let question;
+    export let answerChoice1;
+    export let answerChoice2;
+    export let answerChoice3;
+    export let answerChoice4;
+    let correctAnswer = false
+</script>
+
+<div>
+    <h1>{question}</h1>
+    <div>
+<label>
+    <input type="radio" bind:group={correctAnswer} value={1}>{answerChoice1}
+    <input type="radio" bind:group={correctAnswer} value={2}>{answerChoice2}
+    <input type="radio" bind:group={correctAnswer} value={3}>{answerChoice3}
+    <input type="radio" bind:group={correctAnswer} value={4}>{answerChoice4}
+</label>
+</div>
+</div>
--- note-ai-frontend/src/routes/exam/+page.svelte
+++ note-ai-frontend/src/routes/exam/+page.svelte
@@ -4,6 +4,7 @@
 
    // import root level object as a named import
    import { answer } from '$lib/data/testPrompt.json';
+   import QuestionBlock from '../QuestionBlock.svelte';
 
 
 
@@ -19,7 +20,16 @@
    answerChoicesAndQuestion.forEach((q) => {
        console.log(q);
    });
+   for (let i = 0; i < answerChoicesAndQuestion.length; i++) {
+       for (let j = 0; j < answerChoicesAndQuestion[0].length; j++) {
+        const element = answerChoicesAndQuestion[i][j];
+        console.log(element);
+       }
+   }
    console.log(answerChoicesAndQuestion[0][2]);
  </script>
 
 <h> Testing out the page component </h>
+<QuestionBlock question={answerChoicesAndQuestion[0][0]}, answerChoice1={answerChoicesAndQuestion[0][1]}, answerChoice2={answerChoicesAndQuestion[0][2]}, answerChoice3={answerChoicesAndQuestion[0][3]}, answerChoice4={answerChoicesAndQuestion[0][4]}/>
+<QuestionBlock question={answerChoicesAndQuestion[1][0]}, answerChoice1={answerChoicesAndQuestion[1][1]}, answerChoice2={answerChoicesAndQuestion[1][2]}, answerChoice3={answerChoicesAndQuestion[1][3]}, answerChoice4={answerChoicesAndQuestion[1][4]}/>
+<QuestionBlock question={answerChoicesAndQuestion[2][0]}, answerChoice1={answerChoicesAndQuestion[2][1]}, answerChoice2={answerChoicesAndQuestion[2][2]}, answerChoice3={answerChoicesAndQuestion[2][3]}, answerChoice4={answerChoicesAndQuestion[2][4]}/>
