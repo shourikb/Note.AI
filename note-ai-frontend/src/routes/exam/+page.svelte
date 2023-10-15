e2e6b89e8fa055bf43af61db485a87ac75d9dd07
Srijan Ray <srijanray12@gmail.com>
2023-10-14 23:15:27 -0400

    beautified practice quiz page using tailwindcss

--- note-ai-frontend/src/routes/exam/+page.svelte
+++ note-ai-frontend/src/routes/exam/+page.svelte
@@ -31,13 +31,26 @@
    let showAnswers = false;
  </script>
 
-<h> Testing out the page component </h>
+<div class="bg-cover flex justify-center items-center w-screen h-screen" style="background-image: url('/src/lib/v602-nunoon-32-rippednotes.jpg'); height: 900px">
+<div class="w-3/4 h-[60rem] bg-white relative rounded-lg flex flex-col justify-center items-center border-sky-400 border-[18px]">
+<h1 class="flex text-7xl mb-2 font-mono">Practice Quiz Time!!!</h1>
+<div class="mr-5">
 <QuestionBlock question={answerChoicesAndQuestion[0][0]}, answerChoice1={answerChoicesAndQuestion[0][1]}, answerChoice2={answerChoicesAndQuestion[0][2]}, answerChoice3={answerChoicesAndQuestion[0][3]}, answerChoice4={answerChoicesAndQuestion[0][4]}/>
+</div>
+<div class="mr-28">
 <QuestionBlock question={answerChoicesAndQuestion[1][0]}, answerChoice1={answerChoicesAndQuestion[1][1]}, answerChoice2={answerChoicesAndQuestion[1][2]}, answerChoice3={answerChoicesAndQuestion[1][3]}, answerChoice4={answerChoicesAndQuestion[1][4]}/>
+</div>
+<div class="mr-5">
 <QuestionBlock question={answerChoicesAndQuestion[2][0]}, answerChoice1={answerChoicesAndQuestion[2][1]}, answerChoice2={answerChoicesAndQuestion[2][2]}, answerChoice3={answerChoicesAndQuestion[2][3]}, answerChoice4={answerChoicesAndQuestion[2][4]}/>
+</div>
 <p></p>
-<button on:click={() => showAnswers=true}>Show Answers?</button>
+<div class="mt-8">
+    <button on:click={() => showAnswers=true} class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Show Answers?</button>
+</div>
 
 {#if showAnswers}
-    <p> {answerString} </p>
+    <p class="text-lg"> {answerString} </p>
 {/if}
+
+</div>
+</div>
